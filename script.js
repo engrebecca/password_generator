4ac323210a4a917304803dbbd4e32a24890619c3
reng <67654603+engrebecca@users.noreply.github.com>
2020-09-18 15:47:13 -0700

    Layout pseudocode for password generator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Generate button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// When user clicks button, present prompts for password criteria
+
+// Prompt: length (8-128 characters) "How many characters would you like?"
+// Need user validation to ensure between 8-128 char (error message)
+
+// Click OK to confirm including...
+// Need user validation to ensure at least one criteria chosen
+// Prompt: character types (lower, upper, numeric, and/or special)
+
+// Validate so at least one character type selected
+
+// Generate password with for loop and function
+
+// Display generated password in box
