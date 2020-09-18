09338e16f82c68b5a275c2fade3302055d8b969b
reng <67654603+engrebecca@users.noreply.github.com>
2020-09-18 15:55:43 -0700

    Establish variables for all prompts

--- script.js
+++ script.js
@@ -17,10 +17,23 @@ generateBtn.addEventListener("click", writePassword);
 
 // Prompt: length (8-128 characters) "How many characters would you like?"
 // Need user validation to ensure between 8-128 char (error message)
+var charLen = prompt("How many characters would you like your password to contain?");
+console.log(charLen);
 
 // Click OK to confirm including...
 // Need user validation to ensure at least one criteria chosen
 // Prompt: character types (lower, upper, numeric, and/or special)
+var charLower = confirm("Click confirm to include lowercase letters.");
+console.log(charLower);
+
+var charUpper = confirm("Click confirm to include uppercase letters.");
+console.log(charUpper);
+
+var charNum = confirm("Click confirm to include numbers.");
+console.log(charNum);
+
+var charSpec = confirm("Click confirm to include special characters.");
+console.log(charSpec);
 
 // Validate so at least one character type selected
 
