0d039f4fec12d388402ab0792f21471296722cdb
reng <67654603+engrebecca@users.noreply.github.com>
2020-09-19 10:42:53 -0700

    Set while loop for user prompts

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// When user clicks button, present prompts for password criteria
 
 // VARIABLES
 var pwdNeed = true;
-var charLower = false;
-var charUpper = false;
-var charNum = false;
-var charSpec = false;
+console.log(pwdNeed)
+// var charLower = false;
+// var charUpper = false;
+// var charNum = false;
+// var charSpec = false;
 
 // FUNCTIONS
 function randLower(){
@@ -49,41 +49,40 @@ console.log(randSpec());
 
 
 // While loop
-// while (pwdNeed = true){
-//   // Prompt user to enter desired password length
-//   charLen = prompt("Enter desired password length. Choose between 8-128 characters.");
-//   console.log(charLen);
+while (pwdNeed == true){
+  // Prompt user to enter desired password length
+  charLen = prompt("Enter desired password length. Choose between 8-128 characters.");
+  console.log(charLen);
 
-//   // If letter is between 8-128 run the following
-//   if (charLen > 8 && charLen < 128){
-//     // Run all the other prompts
-//     pwdNeed = false;
-//     // Run all the other character prompts
-//     // Prompt: character types (lower, upper, numeric, and/or special)
-//     var charLower = confirm("Click OK to include lowercase letters.");
-//     console.log(charLower);
+  // If letter is between 8-128 run the following
+  if (charLen > 8 && charLen < 128){
+    // Run all the other prompts
+    pwdNeed = false;
+    // Run all the other character prompts
+    // Prompt: character types (lower, upper, numeric, and/or special)
+    var charLower = confirm("Click OK to include lowercase letters.");
+    console.log(charLower);
 
-//     var charUpper = confirm("Click OK to include uppercase letters.");
-//     console.log(charUpper);
+    var charUpper = confirm("Click OK to include uppercase letters.");
+    console.log(charUpper);
 
-//     var charNum = confirm("Click OK to include numbers.");
-//     console.log(charNum);
+    var charNum = confirm("Click OK to include numbers.");
+    console.log(charNum);
 
-//     var charSpec = confirm("Click OK to include special characters.");
-//     console.log(charSpec);
+    var charSpec = confirm("Click OK to include special characters.");
+    console.log(charSpec);
 
-// // Validate so at least one character type selected
+// Validate so at least one character type selected
 
-// // Generate password with for loop and function
+// Generate password with for loop and function
 
-// // Display generated password in box
-//   } 
-//   // Else run the following error message and redirect to choose valid length
-//   else {
-//     prompt("Please select a length between 8-128 characters.");
-//     pwdNeed = true;
-//   }
-// }
+// Display generated password in box
+  } 
+  // Else run the following error message and redirect to choose valid length
+  else {
+    prompt("Please select a length between 8-128 characters.");
+  }
+}
 
 
 
